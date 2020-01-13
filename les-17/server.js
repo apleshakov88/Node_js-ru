cb09d57866fb0bf9e9d517f7ae7afc4d4e7f0fab
aleksandr.pleshakov <aleksandr.pleshakov@valtech.com>
2020-01-13 19:18:13 +0200

    les-17 Added supervisor npm

---
+++ les-17/server.js
@@ -0,0 +1,31 @@
+// supervisor server.js
+
+const http = require('http');
+const url = require('url');
+
+let counter = 0;
+const server = http.createServer((req, res) => {
+  const { method, url: reqUrl } = req; 
+  const { query, pathname } = url.parse(reqUrl, true);
+  if (method === 'GET' && pathname === '/echo' && query.message) {
+    counter++;
+    // res.statusCode = 200; // OK - 200 --- by default
+    // res.writeHead(200, 'OK', {'Cache-control': 'no-cache'}); // write header right now
+    res.setHeader('Cache-control', 'no-cache'); // write header at nearest write command --- res.end
+    res.end(`${query.message} >>> ${counter}`);
+  } else {
+    res.statusCode = 404;
+    res.end('Page not found');
+  }
+});
+
+
+const serverOrigEmit = server.emit;
+server.emit = (event, ...args) => {
+  console.log(`server emit event decorator >>> ${event}`);
+  return serverOrigEmit.apply(server, [event, ...args])
+}
+
+
+server.listen('1337');
+
